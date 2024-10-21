9cd13f06549fb8515852e0101b8b639f41c2b78d
fdoom <11jjklol@naver.com>
2024-10-21 10:17:00 +0900

    fix: 허용된 Domain만 적용

--- image-api/src/main/java/cyou/noteit/image_server/global/config/WebConfig.java
+++ image-api/src/main/java/cyou/noteit/image_server/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package cyou.noteit.image_server.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${ALLOW_DOMAIN}")
+    private String ALLOW_DOMAIN;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns(ALLOW_DOMAIN)
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true)
